65b16c5bd3ac521415f8acd1463db68328f69278
Tony <toto515891@gmail.com>
2025-07-15 05:54:20 +0800

    crawlPage recursive

--- crawl.js
+++ crawl.js
@@ -3,25 +3,47 @@ const { JSDOM } = require('jsdom')
 
 
 
-async function crawlPage(currentURL){
+async function crawlPage(baseURL, currentURL, pages){
+
+    
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if (currentURLObj.hostname !== baseURLObj.hostname){
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if (pages[normalizedCurrentURL] > 0){
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
     console.log(`actively crawling: ${currentURL}`)
+
     try {
         const resp = await fetch(currentURL)
         if (resp.status > 399){
             console.log(`error in fetch with status code: ${resp.status}, on page: ${currentURL}`)
-            return
+            return pages
         }
 
         const contentType = resp.headers.get("content-type")
         if (!contentType.includes("text/html")){
             console.log(`not html response, content-type: ${contentType}, on page: ${currentURL}`)
-            return
+            return pages
         }
 
-        console.log(await resp.text())
+        const htmlBody = await resp.text()
+        const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+
+        for (const nextURL of nextURLs){
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
     } catch(err){
         console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
     }
+    return pages
 }
 
 
@@ -64,5 +86,4 @@ module.exports = {
     normalizeURL,
     getURLsFromHTML,
     crawlPage
-    
 }
